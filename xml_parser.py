20fbdd784c4ab449d74b8f3aca4418302e52479f
alexcogojocaru <alex.cojocaru73@gmail.com>
2020-10-14 17:36:25 +0300

    Adding the XMLParser class that manages the tags given in web_pages.xml.
    Signed-off-by: alexcogojocaru <alex.cojocaru73@gmail.com>

---
+++ xml_parser.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+
+
+class XMLParser:
+    def __init__(self, xml_file):
+        self.tags = list()
+        self.__parse_xml(xml_file)
+
+    def __parse_xml(self, xml_file):
+        root = ET.parse(xml_file).getroot()
+        for element in root.find('seller'):
+            product = element.find('product')
+
+            self.tags.append({
+                'seller_name'   : element.find('seller_name').text,
+                'product_name'  : product.find('name').text,
+                'link'          : product.find('link').text,
+                'div'           : product.find('div_name').text,
+                'status'        : product.find('status').text,
+                'tag'           : product.find('tag').text,
+                'in_stock'      : product.find('in_stock').text,
+                'out_stock'     : product.find('out_stock').text
+            })
